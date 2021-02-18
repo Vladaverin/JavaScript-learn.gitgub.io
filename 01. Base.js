b8efc9bb9efd84244547f717913a27c07f6fc263
VladAverin <Vladaverin@users.noreply.github.com>
2021-02-18 22:50:01 +0300

    correction1

--- 01. Base.js
+++ 01. Base.js
@@ -9,12 +9,12 @@
 // constanta (const) - переменная, которую нельзя изменять
 // let - новая альтернатива var
 
-// const firstName = 'Vlad' //String
-// const lastName = 'Averin' //String
-// const age = 20 //number
-// const isProgrammer = true //boolean
+const firstName = 'Vlad' //String
+const lastName = 'Averin' //String
+const age = 20 //number
+const isProgrammer = true //boolean
 
-// console.log(isProgrammer)
+console.log(isProgrammer)
 
 // Зарезервированные слова для названий переменных - if, byte, short, int, long.
 // Нельзя начинать название переменной с числа
@@ -22,10 +22,10 @@
 
 // ========================================== 2 Мутирование типов данных
 
-// console.log('Имя человка: ' + firstName + ', а возраст: ' + age)
+console.log('Имя человка: ' + firstName + ', а возраст: ' + age)
 
-// const lastName = prompt('Введите фамилию')
-// alert(firstName + ' ' + lastName)
+const lastName = prompt('Введите фамилию')
+alert(firstName + ' ' + lastName)
 
 // ========================================================= 3 Операторы
 // Операторы математические: + - * /
@@ -33,192 +33,192 @@
 // --(переменная) или (переменная)-- - декрементация значения
 // 'c += a' = 'c = c + a' - сокращенная запись
 
-// let currentYear = 2020
-// const birthYear = 2000
+let currentYear = 2020
+const birthYear = 2000
 
-// const age = currentYear - birthYear
+const age = currentYear - birthYear
 
-// const a = 10
-// const b = 5
+const a = 10
+const b = 5
 
-// let c = 32
-// c = c + a
-// c += a
+let c = 32
+c = c + a
+c += a
 
-// console.log(c)
+console.log(c)
 
-// console.log(a + b)
-// console.log(a - b)
-// console.log(a / b)
-// console.log(a * b)
-// console.log(++currentYear)
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
+console.log(++currentYear)
 
 // ======================================================= 4 Типы данных
 
-// // Примитивы
-// const isProgrammer = true
-// const firstName = 'Vlad'
-// const age = 20
-// let x
-// null
-// // ----------
+// Примитивы
+const isProgrammer = true
+const firstName = 'Vlad'
+const age = 20
+let x
+null
+// ----------
 
-// console.log(typeof isProgrammer) // boolean
-// console.log(typeof firstName) // string
-// console.log(typeof age) // number
-// console.log(typeof x) // undefind
-// console.log(typeof null) //object (БАГ)
-// console.log(null) //null
+console.log(typeof isProgrammer) // boolean
+console.log(typeof firstName) // string
+console.log(typeof age) // number
+console.log(typeof x) // undefind
+console.log(typeof null) //object (БАГ)
+console.log(null) //null
 
 // ============================================== 5 Приоритет операторов
 // >, <, >=, <= - операторы сравнения
 
-// const fullAge = 20
-// const birthYear = 2000
-// const currentYear = 2020
+const fullAge = 20
+const birthYear = 2000
+const currentYear = 2020
 
-// const isFullAge = currentYear - birthYear >= fullAge
+const isFullAge = currentYear - birthYear >= fullAge
 
-// console.log(isFullAge)
+console.log(isFullAge)
 
 // ================================================ 6 Условные операторы
 
-// const courseStatus = 'fail' //ready, fail, pending
+const courseStatus = 'fail' //ready, fail, pending
 
-// if (courseStatus === 'ready') {
-//     console.log('Курс готов, и его можно потреблять')
+if (courseStatus === 'ready') {
+    console.log('Курс готов, и его можно потреблять')
 
-// } else if (courseStatus === 'pending') {
-//     console.log('Курс в разработке, он пока что не готов')
+} else if (courseStatus === 'pending') {
+    console.log('Курс в разработке, он пока что не готов')
 
-// } else {
-//     console.log('Курс не получился')
-// }
+} else {
+    console.log('Курс не получился')
+}
 
-// const isReady = true
+const isReady = true
 
-// if (isReady) {
-//     console.log("Все готово!") // Можно написать без ===
-// } else {
-//     console.log('Все не готово!')
-// }
+if (isReady) {
+    console.log("Все готово!") // Можно написать без ===
+} else {
+    console.log('Все не готово!')
+}
 
 
 // Другая запись примера выше (тернарное выражение)
-// isReady ? console.log('Все готово') : console.log('Все не готово')
+isReady ? console.log('Все готово') : console.log('Все не готово')
 
-// const num1 = 42 //number
-// const num2 = '42' //string
+const num1 = 42 //number
+const num2 = '42' //string
 
-// console.log(num1 === num2)
+console.log(num1 === num2)
 
 
 // ==================================================== 7 Булевая логика
 
 // Логический оператор && (И) если одно из значений false, то будет false.
 
-// console.log(true && false) //false
-// console.log(false && true) //false
-// console.log(false && false) //false
-// console.log(true && true) //true
+console.log(true && false) //false
+console.log(false && true) //false
+console.log(false && false) //false
+console.log(true && true) //true
 
 // Логический оператор || (ИЛИ) если одно из значений true, то будет true.
 
-// console.log(true || false) // true
-// console.log(false || true) // true
-// console.log(true || true) // true
-// console.log(false || false) // false
+console.log(true || false) // true
+console.log(false || true) // true
+console.log(true || true) // true
+console.log(false || false) // false
 
 // Логический оператор ! (НЕ) выводит противоположное булевое значение
 // Можно применять несколько раз
 
-// console.log(!true) //false
-// console.log(!false) //true
+console.log(!true) //false
+console.log(!false) //true
 
 // =========================================================== 8 Функции
 
-// function calculatedAge(year) {
-//     return 2020 - year
-// }
+function calculatedAge(year) {
+    return 2020 - year
+}
 
-// // console.log(calculatedAge(2000))
+// console.log(calculatedAge(2000))
 
-// function logInfoAbout(name, year) {
-//     const age = calculatedAge(year)
+function logInfoAbout(name, year) {
+    const age = calculatedAge(year)
 
-//     if (age > 0) {
-//         console.log('Человек по имени ' + name + ' имеет возраст ' + age)
+    if (age > 0) {
+        console.log('Человек по имени ' + name + ' имеет возраст ' + age)
 
-//     } else if (age < 0) {
-//         console.log('Человек по имени ' + name + ' указал не правильную дату рождения')
+    } else if (age < 0) {
+        console.log('Человек по имени ' + name + ' указал не правильную дату рождения')
 
-//     }
-// }
+    }
+}
 
-// logInfoAbout('Vlad', 2021)
+logInfoAbout('Vlad', 2021)
 
 // =========================================================== 9 Массивы
 
-// const cars = ['Mazda', 'Mersedes', 'Ford'] // Правильный вариант
+const cars = ['Mazda', 'Mersedes', 'Ford'] // Правильный вариант
 
-// // const cars = new Array('Мазда', 'Мерседес', 'Форд') // Не правильный вариант
+// const cars = new Array('Мазда', 'Мерседес', 'Форд') // Не правильный вариант
 
-// console.log(cars.length) // Узнаем длинну массива (колл. элементов)
+console.log(cars.length) // Узнаем длинну массива (колл. элементов)
 
-// console.log(cars[1]) // Обращаемся к индексу массива
-// console.log(cars[0]) // Обращаемся к индексу массива
-// console.log(cars[2]) // Обращаемся к индексу массива
+console.log(cars[1]) // Обращаемся к индексу массива
+console.log(cars[0]) // Обращаемся к индексу массива
+console.log(cars[2]) // Обращаемся к индексу массива
 
-// cars[0] = 'Porsche'
-// cars[3] = 'Mazda'
+cars[0] = 'Porsche'
+cars[3] = 'Mazda'
 
-// console.log(cars)
+console.log(cars)
 
 // ============================================================ 10 Циклы
 
-// const cars = ['Mazda', 'Mersedes', 'Ford', 'Porsche']
+const cars = ['Mazda', 'Mersedes', 'Ford', 'Porsche']
 
-// for (let i = 0; i < cars.length; i++) { // 1 вариант записи
+for (let i = 0; i < cars.length; i++) { // 1 вариант записи
 
-//     const car = cars[i]
-//     console.log(car)
+    const car = cars[i]
+    console.log(car)
 
-// }
+}
 
-// for (car of cars) { // 2 вариант записи
+for (car of cars) { // 2 вариант записи
 
-//     console.log(car)
+    console.log(car)
 
-// }
+}
 
 // ========================================================== 11 Объекты
 
-// // Объект!
-// const person = {
-//     firstName: 'Veronika',
-//     lastName: 'Shkyrskaya',
-//     year: '1999',
-//     languages: ['Ru', 'Eng', 'Blr'],
-//     hasWife: false,
-//     greet: function () { // Добавление функции в объект
-//         console.log('Привет')
-//     }
-// }
+// Объект!
+const person = {
+    firstName: 'Veronika',
+    lastName: 'Shkyrskaya',
+    year: '1999',
+    languages: ['Ru', 'Eng', 'Blr'],
+    hasWife: false,
+    greet: function () { // Добавление функции в объект
+        console.log('Привет')
+    }
+}
 
 
-// // Вариант, как можно обращаться к элементам объекта
+// Вариант, как можно обращаться к элементам объекта
 
-// person.greet() // без console.log
-// console.log(person.firstName) // через точку (.firstName)
-// console.log(person['lastName']) // через скобки ['string']
+person.greet() // без console.log
+console.log(person.firstName) // через точку (.firstName)
+console.log(person['lastName']) // через скобки ['string']
 
-// //
+//
 
-// const key = 'languages' // задать переменную
-// console.log(person[key]) // делается для изменения данных
+const key = 'languages' // задать переменную
+console.log(person[key]) // делается для изменения данных
 
-// person.hasWife = true //изменение данных объекта за его пределами
-// person.isProgrammer = true // Добавление данных в объект за его пределами
+person.hasWife = true //изменение данных объекта за его пределами
+person.isProgrammer = true // Добавление данных в объект за его пределами
 
 
 
--- 02. Num.js
+++ 02. Num.js
@@ -1,80 +1,80 @@
-// // ==================== Number + BigInt + Math ======================
+// ==================== Number + BigInt + Math ======================
 
-// const num = 42
-// const float = 42.42 // Float - число содержит десятичную часть
-// const pow = 10e3 // pow - число в степени (10 в 3 степени)
+const num = 42
+const float = 42.42 // Float - число содержит десятичную часть
+const pow = 10e3 // pow - число в степени (10 в 3 степени)
 
-// console.log('MAX_SAFE_INTEGER ', Number.MAX_SAFE_INTEGER) //Максимальное число для работы
-// console.log('MIN_SAFE_INTEGER ', Number.MIN_SAFE_INTEGER) //Минимальное число для работы
-// console.log('Math.pow ', Math.pow(2, 53) - 1) // Максимальное число в степени
+console.log('MAX_SAFE_INTEGER ', Number.MAX_SAFE_INTEGER) //Максимальное число для работы
+console.log('MIN_SAFE_INTEGER ', Number.MIN_SAFE_INTEGER) //Минимальное число для работы
+console.log('Math.pow ', Math.pow(2, 53) - 1) // Максимальное число в степени
 
 
-// console.log('MAX_VALUE ', Number.MAX_VALUE) // Максимальное число для результата работы
-// console.log('MIN_VALUE ', Number.MIN_VALUE) // Минимальное значение для результата работы
+console.log('MAX_VALUE ', Number.MAX_VALUE) // Максимальное число для результата работы
+console.log('MIN_VALUE ', Number.MIN_VALUE) // Минимальное значение для результата работы
 
-// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY) // Бесконечность
-// console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY) // Бесконечность отрицательная
+console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY) // Бесконечность
+console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY) // Бесконечность отрицательная
 
-// console.log(Number.NaN) // Not A Number (является числом)
-// console.log(2 / undefined) // NaN
+console.log(Number.NaN) // Not A Number (является числом)
+console.log(2 / undefined) // NaN
 
-// const weird = (2 / undefined) // NaN
-// console.log('isNaN weird ', Number.isNaN(weird)) // true
-// console.log('isFinity Infinity ', Number.isFinite(Infinity)) // false
-// console.log('isFinity 42 ', Number.isFinite(42)) // true
+const weird = (2 / undefined) // NaN
+console.log('isNaN weird ', Number.isNaN(weird)) // true
+console.log('isFinity Infinity ', Number.isFinite(Infinity)) // false
+console.log('isFinity 42 ', Number.isFinite(42)) // true
 
-// const stringInt = '40'
-// const stringFloat = '40.40'
-// console.log(stringInt + 2) // JS конкетенирует (складывает строки) все в строку
+const stringInt = '40'
+const stringFloat = '40.40'
+console.log(stringInt + 2) // JS конкетенирует (складывает строки) все в строку
 
-// // Варианты преобразования строки в число (с целыми числами)
-// console.log(Number.parseInt(stringInt) + 2)
-// console.log(Number(stringInt) + 2)
-// console.log(+stringInt + 2)
-// //
+// Варианты преобразования строки в число (с целыми числами)
+console.log(Number.parseInt(stringInt) + 2)
+console.log(Number(stringInt) + 2)
+console.log(+stringInt + 2)
+//
 
-// // Варианты преобразования строки в число (с десятичными элементами)
-// console.log(Number.parseFloat(stringFloat) - 0.4)
-// console.log(Number(stringFloat) - 0.4)
-// console.log(+stringFloat - 0.4)
+// Варианты преобразования строки в число (с десятичными элементами)
+console.log(Number.parseFloat(stringFloat) - 0.4)
+console.log(Number(stringFloat) - 0.4)
+console.log(+stringFloat - 0.4)
 
-// console.log(0.4 + 0.2) //0.6000000000000001
-// console.log((4 / 10) + (2 / 10)) //0.6000000000000001
-// console.log((0.4 + 0.2).toFixed(1)) //0.6
-// console.log(+(0.4 + 0.2).toFixed(1)) // Приводит строчку выше в число
+console.log(0.4 + 0.2) //0.6000000000000001
+console.log((4 / 10) + (2 / 10)) //0.6000000000000001
+console.log((0.4 + 0.2).toFixed(1)) //0.6
+console.log(+(0.4 + 0.2).toFixed(1)) // Приводит строчку выше в число
 
-// // =========================== BigInt ===============================
+// =========================== BigInt ===============================
 
 // Нужен он для работы с числами, которые больше MAX_SAFE_INTEGER
 // BigInt работает только с BigInt
 
-// console.log(900719925474099199999999n - 900719925474099199999999n) //0n
+console.log(900719925474099199999999n - 900719925474099199999999n) //0n
 // console.log(900719925474099199999999.23n) //Error
-// // console.log(4n - 2) //Error
-// console.log(parseInt(4n) - 2) //2
-// console.log(4n - BigInt(2)) //2n
-// console.log(5n / 2n) //2n - BigInt только целое число
+// console.log(4n - 2) //Error
+console.log(parseInt(4n) - 2) //2
+console.log(4n - BigInt(2)) //2n
+console.log(5n / 2n) //2n - BigInt только целое число
 
-// // ============================= Math ===============================
+// ============================= Math ===============================
 
-// console.log(Math.E) // 2.718281828459045
-// console.log(Math.PI) // 3.141592653589793
-// console.log(Math.sqrt(25)) // 5 - корень из числа
-// console.log(Math.pow(5, 3)) // 125 - возведение вzx степень
-// console.log(Math.abs(-42)) // 42 - модуль числа
-// console.log(Math.max(32, 84, 12, 44)) // 84 - макс число
-// console.log(Math.min(32, 84, 12, 44)) // 12 - мин число
-// console.log(Math.floor(199.99)) // 199 - округляет в меньшую сторону
-// console.log(Math.ceil(199.99)) // 200 - округляет в большую сторону
-// console.log(Math.round(199.99)) // 200 - округляет в ближайшую сторону
-// console.log(Math.trunc(199.99)) // 199 - возвращает целое число
-// console.log(Math.random(199.99)) // 199 - возвращает целое число
+console.log(Math.E) // 2.718281828459045
+console.log(Math.PI) // 3.141592653589793
+console.log(Math.sqrt(25)) // 5 - корень из числа
+console.log(Math.pow(5, 3)) // 125 - возведение вzx степень
+console.log(Math.abs(-42)) // 42 - модуль числа
+console.log(Math.max(32, 84, 12, 44)) // 84 - макс число
+console.log(Math.min(32, 84, 12, 44)) // 12 - мин число
+console.log(Math.floor(199.99)) // 199 - округляет в меньшую сторону
+console.log(Math.ceil(199.99)) // 200 - округляет в большую сторону
+console.log(Math.round(199.99)) // 200 - округляет в ближайшую сторону
+console.log(Math.trunc(199.99)) // 199 - возвращает целое число
+console.log(Math.random(199.99)) // 199 - возвращает целое число
 
-// // ===================== Math (практика) ============================
+// ===================== Math (практика) ============================
 
 // Функция выдает рандомное число при внесении мин и макс значения
-// function getRandomBetween(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min)
-// }
-// console.log(getRandomBetween(1, 100))
+function getRandomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+console.log(getRandomBetween(1, 100))
 
